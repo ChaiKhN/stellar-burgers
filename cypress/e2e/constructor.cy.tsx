cb6af4e6afafb3ca25991ccea2c59303fa458996
ChaiKhN <156864798+ChaiKhN@users.noreply.github.com>
2025-06-29 19:55:12 +0300

    fixed after review 1

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -4,15 +4,35 @@ describe('Конструктор бургера', () => {
     cy.visit('/');
   });
 
-  it('добавляет ингредиент в конструктор', () => {
-    cy.get('[data-cy="ingredient-item"]').first().trigger('dragstart');
-    cy.get('[data-cy="constructor-area"]').trigger('drop');
-    cy.get('[data-cy="constructor-area"] .ingredient-item').should('exist');
+  afterEach(() => {
+    cy.clearCookies();
+    cy.window().then((win) => {
+      win.localStorage.clear();
+    });
   });
 
-  it('открывает и закрывает модальное окно ингредиента', () => {
-    cy.get('[data-cy="ingredient-item"]').first().click();
+  it('добавляет конкретный ингредиент в конструктор', () => {
+    const ingredientName = 'Флюоресцентная булка R2-D3';
+
+    cy.get('[data-cy="constructor-area"]').should('not.contain', ingredientName);
+
+    cy.contains('[data-cy="ingredient-item"]', ingredientName)
+      .as('targetIngredient')
+      .trigger('dragstart');
+
+    cy.get('[data-cy="constructor-area"]').trigger('drop');
+
+    cy.get('[data-cy="constructor-area"]').should('contain', ingredientName);
+  });
+
+  it('открывает модалку с данными выбранного ингредиента', () => {
+    const ingredientName = 'Флюоресцентная булка R2-D3';
+
+    cy.contains('[data-cy="ingredient-item"]', ingredientName).click();
+
     cy.get('[data-cy="modal"]').should('exist');
+    cy.get('[data-cy="modal"]').should('contain', ingredientName);
+
     cy.get('[data-cy="modal-close"]').click();
     cy.get('[data-cy="modal"]').should('not.exist');
   });
@@ -23,7 +43,9 @@ describe('Конструктор бургера', () => {
       win.localStorage.setItem('refreshToken', 'fake-refresh-token');
     });
 
-    cy.get('[data-cy="ingredient-item"]').eq(0).trigger('dragstart');
+    const ingredientName = 'Флюоресцентная булка R2-D3';
+
+    cy.contains('[data-cy="ingredient-item"]', ingredientName).trigger('dragstart');
     cy.get('[data-cy="constructor-area"]').trigger('drop');
 
     cy.intercept('POST', '**/orders', {
@@ -32,6 +54,7 @@ describe('Конструктор бургера', () => {
 
     cy.get('[data-cy="place-order"]').click();
     cy.wait('@createOrder');
+
     cy.get('[data-cy="order-number"]').should('contain', '12345');
 
     cy.get('[data-cy="modal-close"]').click();
